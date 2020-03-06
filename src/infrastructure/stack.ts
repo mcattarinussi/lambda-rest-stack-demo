c0c80ec0cf30f26cea09fdca6b22911956cef091
Mattia Cattarinussi <mattia.cattarinussi@dazn.com>
2020-03-06 13:15:20 +0000

    Add api gateway

--- src/infrastructure/stack.ts
+++ src/infrastructure/stack.ts
@@ -1,14 +1,25 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { LambdaRestApi, LambdaIntegration } from '@aws-cdk/aws-apigateway';
 
 export class LambdaRestStackDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'hey', {
+    const heyLambda = new lambda.Function(this, 'hey', {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset('/Users/mattia.cattarinussi/code/github.com/mcattarinussi/lambda-rest-stack-demo/dist/handlers'),
     });
+
+    const heyRestApi = new LambdaRestApi(this, 'helloWorldLambdaRestApi', {
+      restApiName: 'Hello World API',
+      handler: heyLambda,
+      proxy: false,
+    });
+
+    heyRestApi.root
+      .addResource('hey')
+      .addMethod('GET', new LambdaIntegration(heyLambda));
   }
 }
