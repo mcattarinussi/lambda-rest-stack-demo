b005619596134a8fb900c53babd6938db759972b
Mattia Cattarinussi <mcattarinussi@gmail.com>
2020-04-09 20:34:07 +0200

    Parametrize dynamodb table name for local development

--- local/run.sh
+++ local/run.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+DYNAMODB_ENDPOINT=${DYNAMODB_ENDPOINT:-http://host.docker.internal:8000}
+DYNAMODB_TABLE_NAME=${DYNAMODB_TABLE_NAME:-todos-local}
+
 SCRIPT_DIR_FULL_PATH="$PWD/$(dirname $0)"
 
 rm -rf dist cdk.out
@@ -10,14 +13,14 @@ npm run build:dev
 
 npm run cdk:synth -- \
     --no-staging \
-    --context awsEndpoint=http://host.docker.internal:8000 \
-              dynamoTableArn=arn:aws:dynamodb:us-east-1:123456789012:table/todos-local \
-              dynamoTableName=todos-local
+    --context awsEndpoint=$DYNAMODB_ENDPOINT \
+              dynamoTableArn=arn:aws:dynamodb:us-east-1:123456789012:table/$DYNAMODB_TABLE_NAME \
+              dynamoTableName=$DYNAMODB_TABLE_NAME
 
 npm run build:dev:watch &
 PIDS[0]=$!
 
-docker-compose -f $SCRIPT_DIR_FULL_PATH/docker-compose.yml up &
+DYNAMODB_TABLE_NAME=$DYNAMODB_TABLE_NAME DYNAMODB_ENDPOINT=$DYNAMODB_ENDPOINT docker-compose -f $SCRIPT_DIR_FULL_PATH/docker-compose.yml up &
 PIDS[1]=$!
 
 trap "kill ${PIDS[*]}" SIGINT
